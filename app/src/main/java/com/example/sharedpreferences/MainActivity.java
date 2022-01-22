4466a653c37422f76b9de18c5f71fd6789dc7095
chuman <chumanashrestha@gmail.com>
2022-01-22 13:01:17 +0545

    optimize codes

--- app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -12,8 +12,10 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-EditText email, password;
-Button login, signup;
+private EditText email, password;
+private Button login, signup;
+private SharedPreferences sp,lp;
+private Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +24,10 @@ Button login, signup;
         password= findViewById(R.id.etPassword);
         login = findViewById(R.id.btnLogin);
         signup = findViewById(R.id.btnSignup);
+
+        //abc is xml file name
+        sp = getSharedPreferences("abc", MODE_PRIVATE);
+        lp = getSharedPreferences("loginstate", MODE_PRIVATE);
 
         login.setOnClickListener(this);
         signup.setOnClickListener(this);
@@ -33,22 +39,25 @@ Button login, signup;
             //TODO: login logic
             String uEmail = email.getText().toString();
             String uPassword = password.getText().toString();
-            //abc is xml file name
-            SharedPreferences sp = getSharedPreferences("abc", MODE_PRIVATE);
             String fEmail =sp.getString("email","");
             String fPassword = sp.getString("password", "");
 
             if(formValidation(uEmail, uPassword)){
                 if(uEmail.equals(fEmail) && uPassword.equals(fPassword)){
                     Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT ).show();
+
+                    SharedPreferences.Editor et = lp.edit();
+                    et.putBoolean("state",true);
+                    et.apply();
+
+                    startIntent(HomeActivity.class);
                 }else {
                     Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT ).show();
                 }
             }
 
         }else if(v.getId()==R.id.btnSignup){
-            Intent iSignup = new Intent(MainActivity.this, SignupActivity.class);
-            startActivity(iSignup);
+            startIntent(SignupActivity.class);
         }
     }
 
@@ -65,5 +74,21 @@ Button login, signup;
             this.password.setError("Password can't be true");
         }
         return valid;
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkLoginStatus();
+    }
+    private void checkLoginStatus(){
+        SharedPreferences lp = getSharedPreferences("loginstate", MODE_PRIVATE);
+        boolean value= lp.getBoolean("state",false);
+        if(value){
+            startIntent(HomeActivity.class);
+        }
+    }
+    private void startIntent(Class activity){
+        i = new Intent(MainActivity.this, activity);
+        startActivity(i);
     }
 }
