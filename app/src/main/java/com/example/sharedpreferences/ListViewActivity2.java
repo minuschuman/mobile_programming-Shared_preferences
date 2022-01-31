0ed0e9947269fbd3b74c7dc29a61df3fab8d823c
chuman <chumanashrestha@gmail.com>
2022-01-31 08:34:27 +0545

    take data from model and display to view

--- app/src/main/java/com/example/sharedpreferences/ListViewActivity2.java
+++ app/src/main/java/com/example/sharedpreferences/ListViewActivity2.java
@@ -3,12 +3,20 @@ package com.example.sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListViewActivity2 extends AppCompatActivity {
-
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view2);
+        list = findViewById(R.id.list);
+        DataModel d =new DataModel();
+        String[] names = d.getNames();
+        String[] address = d.getAddress();
+
+//        list.setAdapter(new CustomAdapter(this, names, address));
+        list.setAdapter(new CustomAdapter(ListViewActivity2.this, names, address));
     }
 }
